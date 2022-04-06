2cace90c055d0c95d1baed4031269482ace0ba77
yurley <yurs.ksf1@gmail.com>
2022-04-06 18:36:20 -0500

    ejemplos de css

---
+++ frontend-developer/script.js
@@ -0,0 +1,5 @@
+let number_1 = prompt("Ingresa el primer numero:", 30);
+let number_2 = prompt("Ingresa el segundo numero:", 60);
+const resulatado = parseInt(number_1) + parseInt(number_2);
+alert('la suma de ' + number_1 + ' + ' + number_2 + ' = ' + resulatado);
+
