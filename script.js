cac9a9fadf9a654f560e96b0c3236d4c0619f8b7
yurley <yurs.ksf1@gmail.com>
2022-04-08 21:07:32 -0500

    ejemplo Js

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("hola mundo")
+// console.log(5+9)
+// console.log(5-9)
+// console.log(5*9)
+// console.log(5/9)
+
+// condicionales 
+alert("Determinar si la persona es menor de edad")
+respuesta = window.prompt("Ingresa tu edad en años", "");
+
+
+if (respuesta > 18) {
+    // esto se ejecuta si la condicion es verdadera
+    alert("Eres mayor de edad")
+
+    console.log("esto se ejecuta si la condicion es verdadera")
+} else {
+    // esto se ejecuta si la condicion es falsa
+    alert("Eres menor de edad")
+
+    console.log("esto se ejecuta si la condicion es falsa")
+}
+
+// ciclos 
