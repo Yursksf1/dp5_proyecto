81ee0db93831cf37e66285424c5dba7bc09ada64
yurley <yurs.ksf1@gmail.com>
2022-05-06 20:52:37 -0500

    first models -- person

--- django_example/mysite/proyecto/models.py
+++ django_example/mysite/proyecto/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=500)
+    age = models.PositiveIntegerField()
+
+
+    def __str__(self):
+        return "{} - {}".format(self.name, self.age)
