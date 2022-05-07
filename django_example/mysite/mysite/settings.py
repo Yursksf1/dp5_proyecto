cb92f62f486f89e1b62495d18daf3c9dde9c6473
yurley <yurs.ksf1@gmail.com>
2022-05-06 19:54:53 -0500

    hola mundo

--- django_example/mysite/mysite/settings.py
+++ django_example/mysite/mysite/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my cutom application
+    'proyecto.apps.ProyectoConfig',
+    'equipo.apps.EquipoConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
