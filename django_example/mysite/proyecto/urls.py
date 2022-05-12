d8fc0458771e7e28b1a8551b42933121a17e39ee
yurley <yurs.ksf1@gmail.com>
2022-05-11 21:22:09 -0500

    define pages

--- django_example/mysite/proyecto/urls.py
+++ django_example/mysite/proyecto/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from proyecto import views
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path("", views.index),
-    path("asdf", views.index),
+    path('pag1/', TemplateView.as_view(template_name="pagina_1.html")),
+    path('pag2/', TemplateView.as_view(template_name="pagina_2.html")),
+    path('pag3/', TemplateView.as_view(template_name="pagina_3.html")),
 ]
