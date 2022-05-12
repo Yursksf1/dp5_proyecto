d8fe1a7c38ba397b97471681bb8db426b681115d
yurley <yurs.ksf1@gmail.com>
2022-05-11 20:29:24 -0500

    work with templates

--- django_example/mysite/proyecto/views.py
+++ django_example/mysite/proyecto/views.py
@@ -7,8 +7,14 @@ from .models import Player
 def index(request):
     personas = Player.objects.all()
 
+    # ejemplo con string:
     messaje = "Lista de jugadores"
     for persona in personas:
         messaje = messaje + "<br> {}".format(persona.name)
 
-    return HttpResponse(messaje)
+    contexto = {
+        "nombre": "Yurley",
+        "nombres": ["yurley",  "camilo", "garcia"]
+    }
+
+    return render(request, 'index.html', contexto)
