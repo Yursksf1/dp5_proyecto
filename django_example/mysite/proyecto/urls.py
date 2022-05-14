6e32342c5106ce44352cfdfda522be41de6b6e02
yurley <yurs.ksf1@gmail.com>
2022-05-13 20:53:14 -0500

    improve page static

--- django_example/mysite/proyecto/urls.py
+++ django_example/mysite/proyecto/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from proyecto import views
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = "app"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('pag1/', TemplateView.as_view(template_name="pagina_1.html"), name="pag1"),
-    path('pag2/', TemplateView.as_view(template_name="pagina_2.html"), name="pag2"),
-    path('pag3/', TemplateView.as_view(template_name="pagina_3.html"), name="pag3"),
-]
+    path('index', TemplateView.as_view(template_name="pagina_1.html"), name="pag1"),
+    path('information', TemplateView.as_view(template_name="pagina_2.html"), name="pag2"),
+    path('galery', TemplateView.as_view(template_name="pagina_3.html"), name="pag3"),
+    path('new', TemplateView.as_view(template_name="pagina_4.html"), name="pag4"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
